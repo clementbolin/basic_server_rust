82e24ca586446b6f8b81addf9f8ca86beafe2fde
Bolin <clement.bolin@epitech.eu>
2020-06-18 16:44:20 +0200

    add(master): multi client + exchange message between client and server

--- src/client/bin/main.rs
+++ src/client/bin/main.rs
@@ -1,10 +1,56 @@
 use std::net::TcpStream;
+use std::io::{Write, Read, stdin, stdout};
+
+fn get_entry() -> String {
+    let mut buf = String::new();
+
+    stdin().read_line(&mut buf);
+    buf.replace("\n", "").replace("\r", "")
+}
+
+fn exchange_server(mut stream: TcpStream) {
+    let stdoutt = std::io::stdout();
+    let mut io = stdoutt.lock();
+    let mut buf = &mut [0; 3];
+
+    println!("Tap Quit for leave");
+    loop {
+        write!(io, "> ");
+        match io.flush() {
+            Ok(_) => {}
+            Err(e) => { println!("{}", e); }
+        };
+        match &*get_entry() {
+            "Quit" => {
+                println!("leave server");
+                return;
+            }
+            input => {
+                write!(stream, "{}\n", input);
+                match stream.read(buf) {
+                    Ok(res) => {
+                        if res < 1 {
+                            println!("Connection with server lost");
+                            return;
+                        }
+                    }
+                    Err(_) => {
+                        println!("Connection with server lost");
+                        return;
+                    }
+                }
+                println!("Server response: {:?}", buf);
+            }
+        }
+    }
+}
 
 fn main() {
     println!("Try to connect in Server...");
     match TcpStream::connect("127.0.0.1:1234") {
-        Ok(_) => {
+        Ok(stream) => {
             println!("successful login !");
+            exchange_server(stream);
         }
         Err(e) => {
             println!("Connection failed ! {}", e);
--- src/server/bin/main.rs
+++ src/server/bin/main.rs
@@ -1,15 +1,62 @@
-use std::net::TcpListener;
+use std::net::{TcpListener, TcpStream};
+use std::io::{Read, Write};
+use std::thread;
+
+fn handle_client(mut stream: TcpStream, ip: &str) {
+    let mut msg: Vec<u8> = Vec::new();
+
+    loop {
+        let mut buf = &mut [0; 10];
+
+        match stream.read(buf) {
+            Ok(res) => {
+                if res < 1 {
+                    println!("Client disconnect {}", ip);
+                    return;
+                }
+                let mut x = 0;
+                for c in buf {
+                    if x >= res {
+                        break;
+                    }
+                    x += 1;
+                    if *c == '\n' as u8 {
+                        println!("message received {}: {}", ip, String::from_utf8(msg).unwrap());
+                        match stream.write(b"ok\n") {
+                            Ok(_) => {}
+                            Err(err) => { println!("{}", err); }
+                        };
+                        msg = Vec::new();
+                    } else {
+                        msg.push(*c);
+                    }
+                }
+            }
+            Err(_) => {
+                println!("Client {} Disconnect", ip);
+                return;
+            }
+        }
+    }
+}
 
 fn main() {
     let listener = TcpListener::bind("localhost:1234").unwrap();
 
     println!("wait client...");
-    match listener.accept() {
-        Ok((_client, addr)) => {
-            println!("New client (ip: {})", addr);
-        }
-        _ => {
-            println!("A client tried to connect...");
-        }
-    };
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                let ip = match stream.peer_addr() {
+                    Ok(addr) => format!("(ip: {})", addr),
+                    Err(_) => "inconnu".to_owned()
+                };
+                println!("New client: {}", ip);
+                thread::spawn(move|| {
+                    handle_client(stream, &*ip)
+                });
+            }
+            Err(_) => { println!("Connection Failed"); }
+        };
+    }
 }
